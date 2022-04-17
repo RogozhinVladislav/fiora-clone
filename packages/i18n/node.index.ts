0ff9f1cf92a3c63c3e26f830ce9ca77174193e2f
Vladislav Rogozhin <rogojinvladislav@gmail.com>
2022-04-17 23:15:06 +0500

    set english

--- packages/i18n/node.index.ts
+++ packages/i18n/node.index.ts
@@ -1,17 +1,18 @@
 // import osLocale from 'os-locale';
 
-import * as zhCN from './zh-CN';
+// import * as zhCN from './zh-CN';
 import * as enUS from './en-US';
 
-const languages = {
-    'zh-CN': zhCN,
-    'en-US': enUS,
-};
+// const languages = {
+//     'zh-CN': zhCN,
+//     'en-US': enUS,
+// };
 
 // const locale = osLocale.sync() || 'en-US';
-const locale = 'en-US';
+// const locale = 'en-US';
 
-export default function i18n(key: keyof typeof enUS | keyof typeof zhCN) {
+export default function i18n(key: keyof typeof enUS) {
     // @ts-ignore
-    return languages[locale][key] || enUS[key] || key;
+    // return languages[locale][key] || enUS[key] || key;
+    return enUS[key] || key;
 }
